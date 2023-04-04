99caf82344852041f6c7cff5995bc8ef4613b815
Tom Le Coney <tleconey@gmail.com>
2023-04-04 18:13:11 -0500

    v1: hook up images with cloudInary

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -6,6 +6,8 @@ import {
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+
 import 'dotenv/config';
 
 const databaseURL =
@@ -44,6 +46,7 @@ export default withAuth(
       // Schema items go in here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       isAccessAllowed: ({ session }) => !!session?.data,
